8e08644a221d26b254d4143219b9c229ceed3333
Luis V. Capelletto <thecapellett@gmail.com>
2022-11-04 14:57:32 -0300

    fix(dialogs page): update tags

--- src/pages/dialogs.tsx
+++ src/pages/dialogs.tsx
@@ -1,8 +1,9 @@
+import { ButtonToggleTheme } from 'components/ButtonToggleTheme'
 import { Dialog } from 'components/Dialog'
 import { DialogAlert } from 'components/DialogAlert'
 import { useTheme } from 'contexts/ThemeContext'
 import Head from 'next/head'
-import { Main } from 'styles/pages/home'
+import { BodyWrapper, Main } from 'styles/pages/home'
 
 export default function Home() {
   const { theme, toggleTheme } = useTheme()
@@ -12,28 +13,30 @@ export default function Home() {
       <Head>
         <title>Dialogs</title>
       </Head>
-      <Main theme={theme}>
-        <h1>Dialogs</h1>
+      <BodyWrapper theme={theme}>
+        <Main>
+          <h1>Dialogs</h1>
 
-        <button onClick={toggleTheme}>Switch</button>
+          <ButtonToggleTheme />
 
-        <DialogAlert
-          triggerLabel="Deletar conta"
-          confirmLabel="Sim, deletar conta"
-          cancelLabel="Cancelar"
-          title="Você tem certeza que deseja deletar sua conta?"
-          description="Essa ação não poderá ser desfeita. Todos os seus dados serão perdidos."
-        />
+          <DialogAlert
+            triggerLabel="Deletar conta"
+            confirmLabel="Sim, deletar conta"
+            cancelLabel="Cancelar"
+            title="Você tem certeza que deseja deletar sua conta?"
+            description="Essa ação não poderá ser desfeita. Todos os seus dados serão perdidos."
+          />
 
-        <Dialog
-          title="Fazer alguma coisa"
-          description="Aqui você pode fazer alguma coisa."
-          triggerLabel="Fazer alguma coisa"
-          confirmLabel="Confirmar"
-        >
-          <p>Conteúdo do dialog</p>
-        </Dialog>
-      </Main>
+          <Dialog
+            title="Fazer alguma coisa"
+            description="Aqui você pode fazer alguma coisa."
+            triggerLabel="Fazer alguma coisa"
+            confirmLabel="Confirmar"
+          >
+            <p>Conteúdo do dialog</p>
+          </Dialog>
+        </Main>
+      </BodyWrapper>
     </>
   )
 }
