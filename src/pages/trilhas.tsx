ae9e441c3220141f9d39829a2b6ecf779c0b291a
Luis V. Capelletto <thecapellett@gmail.com>
2022-11-06 18:11:19 -0300

    refactor(pages): update pages folder structure

---
+++ src/pages/trilhas.tsx
@@ -0,0 +1,22 @@
+import { ButtonToggleTheme } from 'components/ButtonToggleTheme'
+import { useTheme } from 'contexts/ThemeContext'
+import Head from 'next/head'
+import { BodyWrapper, Main } from 'styles/pages/home'
+
+export default function Home() {
+  const { theme } = useTheme()
+
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <BodyWrapper theme={theme}>
+        <Main>
+          <h1>Home</h1>
+          <ButtonToggleTheme />
+        </Main>
+      </BodyWrapper>
+    </>
+  )
+}
